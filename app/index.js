a0aea95c2990dc87e8e9861a4e74e3f062946ba8
João Vitor Camargo <140967915+Jvcamargo1@users.noreply.github.com>
2024-09-26 13:29:17 -0300

    portado de typescript para javascript. adicionado arquivos de som experimentais frontend e backend separados frontend se encontra em app/index.js e backend em src/Soundboard.js a gente tem que renomear esse Soundboard.js pra alguma coisa que faça mais sentido, porém a raiz inicial do projeto esta "pronta"

---
+++ app/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import Soundboard from '../src/Soundboard';  // Caminho ajustado para o Soundboard.js dentro de 'src'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={require('../assets/images/background.png')} 
+        style={styles.backgroundImage} 
+        resizeMode="cover" 
+      />
+      <View style={styles.blueBar}>
+        <Text style={styles.title}>Fala+</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        {/* Renderiza o Soundboard, que contém a lógica dos botões */}
+        <Soundboard />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
+  },
+  blueBar: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#add8e6',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 32,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 0,
+    marginTop: 20,
+  },
+});
+
+export default App;
